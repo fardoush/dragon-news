8cb509847150b6a3bc5e28734476d12d7aa8b3fd
fardoush <fardoushjahanara@gmail.com>
2025-11-24 23:36:22 +0600

    add to header , date and marguee and add to menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
             <h1 className="text-6xl text-center">The Dragon News</h1>
             <p className="text-accent">Journalism Without Fear or Favour</p>
+            <p className="text-600 text-accent">{format(new Date(),"EEEE , MMMM MM , yyyy",)}</p>
             
 
         </div>
