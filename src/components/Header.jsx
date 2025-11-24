d114ccf2f6a6c18f20e20cc3979bd0b68cb140ec
fardoush <fardoushjahanara@gmail.com>
2025-11-24 22:42:18 +0600

    add color theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
             <h1 className="text-6xl text-center">The Dragon News</h1>
-            <p className="text-gray-400">Journalism Without Fear or Favour</p>
+            <p className="text-accent">Journalism Without Fear or Favour</p>
             
 
         </div>
